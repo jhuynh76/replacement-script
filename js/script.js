cec7989878f07e4b2b06d62a6fc3dcc882eed7b9
JHD Solutions <jhuynh776@gmail.com>
2020-03-11 20:05:50 -0400

    Commit 8 - updated the replacement values to include O's, and more U's

--- js/script.js
+++ js/script.js
@@ -1,30 +1,49 @@
 $('#btn').click(function(){
 	var txt = $('#txt').val();
-	var replace =
-		txt.replace(/[À]/g, '&Agrave;')
-			.replace(/[à]/g, '&agrave;')
-			.replace(/[Â]/g, '&Acirc;')
-			.replace(/[â]/g, '&acirc;')
-			.replace(/[Ç]/g, '&Ccedil;')
-			.replace(/[ç]/g, '&ccedil;')
-			.replace(/[È]/g, '&Egrave;')
-			.replace(/[è]/g, '&egrave;')
-			.replace(/[É]/g, '&Eacute;')
-			.replace(/[é]/g, '&eacute;')
-			.replace(/[Ê]/g, '&Ecirc;')
-			.replace(/[ê]/g, '&ecirc;')
-			.replace(/[Î]/g, '&Icirc;')
-			.replace(/[Ô]/g, '&Ocirc;')
-			.replace(/[ô]/g, '&ocirc;')
-			.replace(/[Ù]/g, '&Ugrave;')
-			.replace(/[ù]/g, '&ugrave;')
-			.replace(/[Û]/g, '&Ucirc;')
-			.replace(/[û]/g, '&ucirc;')
-			.replace(/®/g, '<sup style="font-size:10px; line-height:0">&reg;</sup>')
-			.replace(/©/g, '<sup style="font-size:10px; line-height:0">&copy;</sup>')
-			.replace(/“/g, '&ldquo')
-			.replace(/”/g, '&rdquo')
-			.replace(/\n/g, '<br>\r');
+	var replace = txt
+		.replace(/[Á]/g, '&Aacute;')
+		.replace(/[á]/g, '&aacute;')
+		.replace(/[À]/g, '&Agrave;')
+		.replace(/[à]/g, '&agrave;')
+		.replace(/[Â]/g, '&Acirc;')
+		.replace(/[â]/g, '&acirc;')
+
+		.replace(/[É]/g, '&Eacute;')
+		.replace(/[é]/g, '&eacute;')
+		.replace(/[È]/g, '&Egrave;')
+		.replace(/[è]/g, '&egrave;')
+		.replace(/[Ê]/g, '&Ecirc;')
+		.replace(/[ê]/g, '&ecirc;')
+
+		.replace(/[Í]/g, '&Iacute;')
+		.replace(/[í]/g, '&iacute;')
+		.replace(/[Ì]/g, '&Igrave;')
+		.replace(/[ì]/g, '&igrave;')
+		.replace(/[Î]/g, '&Icirc;')
+		.replace(/[î]/g, '&icirc;')
+
+		.replace(/[Ó]/g, '&Oacute;')
+		.replace(/[ó]/g, '&oacute;')
+		.replace(/[Ò]/g, '&Ograve;')
+		.replace(/[ò]/g, '&ograve;')
+		.replace(/[Ô]/g, '&Ocirc;')
+		.replace(/[ô]/g, '&ocirc;')
+
+		.replace(/[Ú]/g, '&Uacute;')
+		.replace(/[ú]/g, '&uacute;')
+		.replace(/[Ù]/g, '&Ugrave;')
+		.replace(/[ù]/g, '&ugrave;')
+		.replace(/[Û]/g, '&Ucirc;')
+		.replace(/[û]/g, '&ucirc;')
+
+		.replace(/[Ç]/g, '&Ccedil;')
+		.replace(/[ç]/g, '&ccedil;')
+
+		.replace(/®/g, '<sup style="font-size:10px;">&reg;</sup>')
+		.replace(/©/g, '<sup style="font-size:10px;">&copy;</sup>')
+		.replace(/“/g, '&ldquo')
+		.replace(/”/g, '&rdquo')
+		.replace(/\n/g, '<br>\r');
 
 	$('#result').val(replace);
 });
